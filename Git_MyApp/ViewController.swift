22945cdb342093fae3c825c093c0c61095c668bc
armangorjipoor <armangorjipoor@gmail.com>
2020-06-21 17:54:31 +0430

    Initial Commit

---
+++ Git_MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git_MyApp
+//
+//  Created by ArmanG on 4/1/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
