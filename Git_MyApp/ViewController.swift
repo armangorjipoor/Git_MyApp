3e567987e0e13b2659594caab85d5dbc77d0158c
armangorjipoor <armangorjipoor@gmail.com>
2020-06-21 18:02:37 +0430

    This is first change in the project

--- Git_MyApp/ViewController.swift
+++ Git_MyApp/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnTap(_ sender: UIButton) {
+        print("Btn Tapped")
+    }
 
 }
 
